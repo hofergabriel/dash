88fd7c4f7fdd927c850252fb4a7744a1503fccab
Gabriel Hofer <leirbagabriel468@gmail.com>
2021-02-25 16:43:06 -0600

    -

--- dash.c
+++ dash.c
@@ -9,9 +9,11 @@
 void cmdnm(char * pid){
   char path[256], scratch[256], nm[256];
   sprintf(path, "/proc/%s/stat", pid);
-  FILE *fp = fopen(path, "r");	
+  FILE * fp = fopen(path, "r");	
+  if(fp==NULL) return;
   fscanf(fp, "%s%s", scratch, nm);
   printf("%s\n", nm);
+  fclose(fp);
 }
 
 void pid(char * command) {
@@ -21,6 +23,7 @@ void pid(char * command) {
   printf("%s\n\n",command2);
 
   for(int i=0;i<13120;i++){
+
     sprintf(pid,"%d",i);
 
     char path[256], scratch[256], nm[256];
@@ -29,12 +32,14 @@ void pid(char * command) {
     printf("path: %s\n",path);
 
     FILE *fp = fopen(path, "r");	
+    if(fp==NULL) return;
     if(fp){ 
       fscanf(fp, "%s%s", scratch, nm);
       printf("pid: %s cmdnm: %s\n",scratch,nm);
       if(!strcmp(command2,nm))
         printf("\t\t%s",nm);
     }
+    fclose(fp);
   }
 }
 
@@ -43,22 +48,41 @@ void cd(char * dir){
     perror("error changing directories\n");
 }
 
+void showfile(char * filename){
+  char * buf=NULL;
+  size_t leng=32;
+  char path[256];
+  sprintf(path, "/proc/%s", filename);
+  FILE * fp = fopen(path,"r");
+  if(fp==NULL) return;
+  while(getline(&buf, &leng, fp) != -1)
+    printf("%s",buf);
+  fclose(fp);
+}
 
+
+void systat(){
+  char nm[] = "meminfo\0cpuinfo\0uptime\0version\0";
+  showfile(nm);
+  showfile(&nm[8]);
+  showfile(&nm[16]);
+  showfile(&nm[23]);
+}
 
 
 // proc/meminfo
 // proc/version
 
 
-
 void REPL(){
   char * buf=NULL;
+  size_t leng=32;
   char a[32], b[32];
   char str1[] = "cmdnm";
   char str2[] = "pid";
   char str3[] = "cd";
   char str4[] = "exit"; 
-  size_t leng=32;
+  char str5[] = "systat";
   char cwd[PATH_MAX];
 
   for(;;){
@@ -71,12 +95,18 @@ void REPL(){
     if(!strcmp(a, str2)) pid(b);
     if(!strcmp(a, str3)) cd(b);
     if(!strcmp(a, str4)) return;
+    if(!strcmp(a, str5)) systat();
+    else {
+      system(buf); 
 
+    }
   }
 }
 
 
+/***********************************************************/
 
+/***********************************************************/
 void main(){
   REPL();
 }
